7c8183800104c4bfe7e1bb2fb9cd46082158a316
Josh Wulf <josh.wulf@camunda.com>
2023-07-27 07:57:53 +1200

    updates

--- CCFR.md
+++ CCFR.md
@@ -1,3 +1,7 @@
 # CCFR: Cause, Consequence, Fix, Result
 
-CCFR is a 
+Here it is as a ChatGPT prompt:
+
+You are Release Notes GPT. You take GitHub issue text and produce a release note for end-users. This release note consists of four parts: a cause, a consequence, a fix, and a result. The "cause" is the underlying cause of the issue, expressed in terms of the technical engineering of the product. The "consequence" is impact of the issue, expressed in the observable behaviour of the system for the user. The "fix" is a description of the technical fix to the product. The "result" is a description of the observable behaviour of the system for the user when this fix is applied.
+
+I will give you the GitHub Issue text, and you will produce a terse paragraph summarising the information in the issue text following the structure: "cause, consequence, fix, result". Is this clear?
--- automation/app.ts
+++ automation/app.ts
@@ -1,7 +1,9 @@
 import {Tasklist} from "camunda-8-sdk"
 import dotenv from "dotenv"
 import express from "express"
+import fs from 'fs'
 import { startDatabaseWorker } from "./worker"
+import paginate from 'express-paginate'
 
 var list = {}
 dotenv.config()
@@ -11,15 +13,34 @@ const t = new Tasklist.TasklistApiClient()
 
 const app = express()
 app.set('view engine', 'pug')
+app.use(paginate.middleware(10, 50));
+
 
 const port = 3000
 
-app.listen(port, () => console.log(`Serving on port 3000...`))
+app.listen(port, () => console.log(`Serving on http://127.0.0.1:3000...`))
+
+const releases = JSON.parse(fs.readFileSync('rel-8.3-report.raw.json', 'utf-8'))
 
 app.get('/', (_, res) => {
     res.render('index', {list})
 })
-
+app.get('/releases', (req, res) => {
+    const limit = parseInt(req.query.limit as any ?? 50)
+    const pageCount = Math.ceil(releases.length / limit)
+    console.log({pageCount})
+    const itemCount = releases.length
+    const skip = req.skip ?? 0
+    const results = releases.slice(skip, +limit)
+    console.log({skip, limit})
+    console.log(results)
+    res.render('release-matrix', {
+        releases,
+        pageCount,
+        itemCount,
+        pages: paginate.getArrayPages(req)(1, pageCount, req.query.page! as any)
+    })
+})
     
 const update = () => t.getTasks({
     state: Tasklist.TaskState.CREATED
@@ -34,9 +55,10 @@ const update = () => t.getTasks({
             return a.length > 1 ? a[a.length -1] : ''
         })(),
         githubIssueUrl: task.variables.githubIssueUrl,
-        tasklistUrl: `${TASKLIST_URL}/${task.processInstanceId}`
+        tasklistUrl: `${TASKLIST_URL}/${task.id}`,
+        releaseNoteType: task.variables.releaseNoteType
     }))
-})
+}).catch(console.error)
 
 update()
 setInterval(update, 10000)
